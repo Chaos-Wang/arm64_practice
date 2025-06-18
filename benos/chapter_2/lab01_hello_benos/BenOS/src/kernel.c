58301c862fb7011d07d059d335e20a352c593343
Chaos-Wang <chaoswang625@gmail.com>
2025-06-18 15:36:04 +0800

    [CHG] 使用汇编实现memset功能,并且实现非对齐访问的特殊处理,qemu不会出现非对齐访问的报错,书上说树莓派上会崩溃,这个暂时无法验证

--- benos/chapter_2/lab01_hello_benos/BenOS/src/kernel.c
+++ benos/chapter_2/lab01_hello_benos/BenOS/src/kernel.c
@@ -2,18 +2,18 @@
 #include "mm.h"
 
 void *unalign_addr = 0x80004;
-void *unalign_addr1 = 0x80002;
+void *unalign_addr1 = 0x80001;
 void *unalign_addr2 = 0x80008;
 
 void kernel_main(void)
 {
-	char src[32] = "Welcome, Benosi\n";
+	char src[132] = "Welcome, Benosi\n";
 	char dst[16];
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
 
 	memcpy(src, dst, 16);
-	char *ret = memset(src, 'c', 32);
+	char *ret = memset(unalign_addr1, 'c', 53);
 	memcpy(unalign_addr, unalign_addr1, 16);
 	memcpy(unalign_addr1, unalign_addr2, 16);
 	memcpy(unalign_addr2, unalign_addr1, 16);
